d671d27a80a8bf6126129b18e2ae13c1e58ec7eb
anayatc@gmail.com <anayatc@sky.com>
2017-05-30 23:23:04 +0100

    basic structure

--- reddit_reader.py
+++ reddit_reader.py
@@ -4,3 +4,15 @@ import json
 import requests
 import time
 import unidecode
+
+app = Flask(__name__)
+ask = Ask(app, "/reddit_reader")
+
+
+def get_headlines():
+    pass
+
+@app.route('/')
+
+if __name__ = '__main__':
+    app.run()
