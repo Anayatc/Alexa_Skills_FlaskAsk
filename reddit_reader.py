f099184f193123b9f25deb4a0c940cdf3b2a29b8
anayatc@gmail.com <anayatc@sky.com>
2017-05-30 23:26:36 +0100

    defined questions

--- reddit_reader.py
+++ reddit_reader.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
 import json
 import requests
 import time
 import unidecode
+
 
 app = Flask(__name__)
 ask = Ask(app, "/reddit_reader")
@@ -12,9 +13,15 @@ ask = Ask(app, "/reddit_reader")
 def get_headlines():
     pass
 
+
 @app.route('/')
 def homepage():
     return "homepage"
 
-if __name__ = '__main__':
+@ask.launch
+def start_skill():
+    welcome_message = 'Hello, Welcome to reddit reader, would you like the reddit headlines?'
+    return question(welcome_message)
+
+if __name__ == '__main__':
     app.run()
