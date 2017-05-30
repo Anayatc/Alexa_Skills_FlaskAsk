cda59f1df9a9f75fae05682d19fa4cf7a2535d12
anayatc@gmail.com <anayatc@sky.com>
2017-05-30 23:30:09 +0100

    defined yes and no intent from user

--- reddit_reader.py
+++ reddit_reader.py
@@ -18,10 +18,19 @@ def get_headlines():
 def homepage():
     return "homepage"
 
+
 @ask.launch
 def start_skill():
     welcome_message = 'Hello, Welcome to reddit reader, would you like the reddit headlines?'
     return question(welcome_message)
+
+
+@ask.intent("YesIntent")
+def share_headlines():
+    headlines = get_headlines()
+    headline_msg = 'the current world news headlines are {}'.format(headlines)
+    return statement(headline_msg
+                     )
 
 if __name__ == '__main__':
     app.run()
